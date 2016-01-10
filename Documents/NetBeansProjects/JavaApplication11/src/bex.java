0deff7bfe2bddd26a0d8c4657ddcf717d733d678
samir02 <samir.pokhrel02@gmail.com>
2016-01-10 21:44:45 +0545

    good one

---
+++ Documents/NetBeansProjects/JavaApplication11/src/bex.java
@@ -0,0 +1,11 @@
+
+public class bex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new bexx().setVisible(true);
+    }
+    
+}
